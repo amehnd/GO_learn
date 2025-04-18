968306002257c9d3c86e8677093eb200ac143fae
Aryan Mehndiratta <aryanmehndiratta166@gmail.com>
2025-04-18 10:55:29 +0000

    add new modules

---
+++ hello/hello.go
@@ -0,0 +1,18 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+//     fmt.Println("Hello, World!")
+// }
+
+
+package main
+
+import "fmt"
+
+import "rsc.io/quote"
+
+func main() {
+    fmt.Println(quote.Go())
+}
